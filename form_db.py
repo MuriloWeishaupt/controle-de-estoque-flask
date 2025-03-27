fa3d655ec1560380cdbc530c40cfd96fcfd9fd4e
Murilo Weishaupt <muriloweishaupt@gmail.com>
2025-03-27 16:25:22 -0300

    Criada conexão e tabela com banco de dados SQLite3

---
+++ form_db.py
@@ -0,0 +1,18 @@
+import sqlite3 as sql
+
+con = sql.connect("form_db.db")
+cur = con.cursor()
+cur.execute("DROP TABLE IF EXISTS produtos")
+
+sql = '''CREATE TABLE "produtos" (
+    "ID" INTEGER PRIMARY KEY AUTOINCREMENT,
+    "NOME" TEXT,
+    "QUANTIDADE_ESTOQUE" INTEGER,
+    "FABRICANTE" TEXT,
+    "DESCRIÇÃO" TEXT,
+    "PRECO" REAL
+    )'''
+
+cur.execute(sql)
+con.commit()
+con.close()
